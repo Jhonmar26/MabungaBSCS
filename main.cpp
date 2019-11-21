f02a34dd2864c8d425ccab13318c55a0f4d85cec
Jarmamu <56722333+Jhonmar26@users.noreply.github.com>
2019-11-21 21:22:49 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,80 +1,57 @@
 #include <iostream>
 #include <stdlib.h>
+
 using namespace std;
 
-int inputArraySize();
-void inputArray(string[],string[],int[],int[],string[],string[]);
-void DisplayArray(string[],string[],int[],int[],string[],string[]);
+class product {
+public:
+    void addproduct (int Product_ID, int Price, int Expiration_Date, string Product_Name, string Manufacturer);
+    void DisplayProducts (void);
+    product();
+private:
+    int Product_ID, Price, Expiration_Date;
+    string Product_Name, Manufacturer;
+    };
 
-int arrSize;
+product::product (void) {
+    string choice;
 
-int main()
+    cout << "WELCOME TO JARMAMU KAINAN" << endl;
+
+
+
+do{
+    cout << " Product ID: ";
+    cin >> Product_ID;
+    cout << " Product Name: ";
+    cin >> Product_Name;
+    cout << " Price: ";
+    cin >> Price;
+    cout << " Manufacturer: ";
+    cin >> Manufacturer;
+    cout << " Expiration Date: ";
+    cin >> Expiration_Date;
+    cout << "Do you want to add products? (Y/N) ";
+    cin >> choice;
+}
+while (choice == "Y" || choice == "y");
+}
+
+void product::DisplayProducts()
 {
-    string lastname[10];
-    string firstname[10];
-    string Email[10];
-    string Address[10];
-    int age [10];
-    int contacts [10];
-    inputArray(firstname,lastname,age,contacts,Email,Address);
-    DisplayArray(firstname,lastname,age,contacts,Email,Address);
+    system ("CLS");
+    system ("Color 2");
+    cout << "The Product ID is: " << Product_ID << endl;
+    cout << "The Product Name is: " << Product_Name << endl;
+    cout << "The Price is: " << Price << endl;
+    cout << "The Product Manufacturer: " << Manufacturer << endl;
+    cout << "The Product Expiration Date is: " << Expiration_Date << endl;
+}
+
+int main(){
+    product product;
+
+    product.DisplayProducts();
 
     return 0;
-}
-
-int inputArraySize ()
-{
-    int age [arrSize];
-
-    cout << "Enter Array Size : ";
-    cin >> arrSize;
-
-    return arrSize;
-}
-
-void inputArray (string firstname[], string lastname[], int age[], int contacts[], string Email[], string Address[])
-{
-    inputArraySize();
-
-    for (int i=0; i<=arrSize-1;i++){
-        cout << "Enter First Name: ";
-        cin >> firstname[i];
-    }
-
-    for (int i=0; i<=arrSize-1;i++){
-        cout << "Enter Last Name: ";
-        cin >> lastname[i];
-    }
-
-    for (int i=0; i<=arrSize-1;i++){
-        cout << "Enter Age: ";
-        cin >> age[i];
-    }
-
-    for (int i=0; i<=arrSize-1;i++){
-        cout << "Enter Contact No.: ";
-        cin >> contacts[i];
-    }
-
-    for (int i=0; i<=arrSize-1;i++){
-        cout << "Enter Your Email: ";
-        cin >> Email[i];
-    }
-
-    for (int i=0; i<=arrSize-1;i++){
-        cout << "Enter Your Address: ";
-        cin >> Address[i];
-    }
-}
-
-void DisplayArray (string firstname[], string lastname[], int age[], int contacts[], string Email[], string Address[])
-
-{
-
-system("CLS");
-    cout << "Firstname" << "|" << "Lastname" << "|" << "Age" << "|" << "Contacts" << "|" << "Email" << "|" << "Address" << endl;
-    for (int i=0; i<=arrSize-1;i++){
-
-        cout << " \n "<< firstname[i]<< "|" << lastname[i] << "|" << age[i] << "|" << contacts[i] << "|" << Email[i] << "|" << Address[i] << endl;
-    }
 }
